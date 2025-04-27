236d19546241eb6bbb4d3492191a2f6fa362f916
Js727-dot <jasminedhanaraj15@gmail.com>
2025-04-27 11:53:45 +0530

    Add files via upload

---
+++ BaseTest.java
@@ -0,0 +1,19 @@
+package Base;
+
+public class BaseTest {
+	public static void main(String[] args) {
+		
+		float versionof = 91.0f;
+		String developerof = "Chrome";
+		boolean isBeta = false;
+		int releaseyear = 2008;
+		char shortcutkey  = 'c';
+		
+		System.out.println("Version" +versionof);
+		System.out.println("Developed in"+developerof);
+		System.out.println("value"+isBeta);
+		System.out.println("Release Year"+ releaseyear);
+		System.out.println("ShortcutKey"+shortcutkey);
+	}
+
+}
